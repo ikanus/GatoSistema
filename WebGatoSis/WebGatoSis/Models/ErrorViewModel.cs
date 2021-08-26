e319209ed004ce759f871c010198d87a4580896f
ikanus <33515638+ikanus@users.noreply.github.com>
2021-08-25 19:03:45 -0500

    Crea proyecto y adapta plantilla
    Se crea un proyecto ASP.NET Core 3.1 MVC y se adapta la plantilla AdminLTE en la plantilla principal y el login.

---
+++ WebGatoSis/WebGatoSis/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebGatoSis.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
