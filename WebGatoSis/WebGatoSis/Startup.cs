005a7a40ae3c4dd42266e394fdc4c6153074e4c7
ikanus <33515638+ikanus@users.noreply.github.com>
2021-08-25 23:03:25 -0500

    Se cambia para que la página Login sea la inicial

--- WebGatoSis/WebGatoSis/Startup.cs
+++ WebGatoSis/WebGatoSis/Startup.cs
@@ -51,7 +51,7 @@ namespace WebGatoSis
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
